af784c7cc06a6a56f350df005ea8fc7a193e9d91
Joohyun kyong <joohyun@kookmin.ac.kr>
2017-02-03 15:30:36 +0900

    update

--- src/test/java/com/sungshin/CalcTest.java
+++ src/test/java/com/sungshin/CalcTest.java
@@ -1,14 +1,39 @@
 package com.sungshin;
 
-import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class CalcTest 
-{
+ 
+public class CalendarioTest {
+    
+    public CalendarioTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+ 
     @Test
     public void testAdd()
     {
         Calc c  = new Calc();
 	assertEquals(30, c.add(10, 20));
     }
+ 
 }
